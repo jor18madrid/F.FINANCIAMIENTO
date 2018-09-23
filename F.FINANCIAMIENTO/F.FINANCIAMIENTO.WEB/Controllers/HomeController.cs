2356d461e643b1602f9263e44dc2a5838c391c63
Jorge Alberto Reyes Rivas <jor18madrid@gmail.com>
2018-09-23 00:14:59 -0600

    Trabajando sin conexion a la base de sefin

--- F.FINANCIAMIENTO/F.FINANCIAMIENTO.WEB/Controllers/HomeController.cs
+++ F.FINANCIAMIENTO/F.FINANCIAMIENTO.WEB/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using F.FINANCIAMIENTO.DA;
+using F.FINANCIAMIENTO.DTO;
 using F.FINANCIAMIENTO.INTERFACES;
 using System;
 using System.Collections.Generic;
@@ -21,14 +22,29 @@ namespace F.FINANCIAMIENTO.WEB.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            var grupos = _fGrupoFiltro.qry_SAS_GRUPOS_Listado();
-            var dicGrpsLst = new Dictionary<int, string>();
-            foreach (var item in grupos)
-            {
-                var id = item;
-                dicGrpsLst.Add(item.ID_GRUPO, item.NOMBRE_GRUPO.ToString());
-            }
-            ViewBag.Grupos = dicGrpsLst.Select(x => new SelectListItem { Value = x.Key.ToString(), Text = x.Value });
+            //var grupos = _fGrupoFiltro.qry_SAS_GRUPOS_Listado();
+            //var dicGrpsLst = new Dictionary<int, string>();
+            //foreach (var item in grupos)
+            //{
+            //    var id = item;
+            //    dicGrpsLst.Add(item.ID_GRUPO, item.NOMBRE_GRUPO.ToString());
+            //}
+            //ViewBag.Grupos = dicGrpsLst.Select(x => new SelectListItem { Value = x.Key.ToString(), Text = x.Value });
+
+
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+            var dicGrpsLst = new List<SAS_GRUPO_FILTRO_DTO>();
+            dicGrpsLst.Add(new SAS_GRUPO_FILTRO_DTO() { ID_GRUPO = 1, NOMBRE_GRUPO = "Grupo 1" });
+            dicGrpsLst.Add(new SAS_GRUPO_FILTRO_DTO() { ID_GRUPO = 2, NOMBRE_GRUPO = "Grupo 2" });
+            ViewBag.Grupos = dicGrpsLst.Select(x => new SelectListItem { Value = x.ID_GRUPO.ToString(), Text = x.NOMBRE_GRUPO });
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
             return View();
         }
     }
