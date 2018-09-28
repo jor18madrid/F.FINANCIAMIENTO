3cfda63554265399b061bc1d3aeb2f89c67167bc
Jorge Alberto Reyes Rivas <jor18madrid@gmail.com>
2018-09-27 22:48:31 -0600

    Empecé los dto pero no los he terminado

---
+++ F.FINANCIAMIENTO/F.FINANCIAMIENTO.DTO/InsertDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F.FINANCIAMIENTO.DTO
+{
+    public class InsertDto
+    {
+        public InsertDto() { }
+
+        List<LstOfInserts> FuenteFin { get; set; }
+    }
+}
