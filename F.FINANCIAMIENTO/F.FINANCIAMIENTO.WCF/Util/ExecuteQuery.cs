507b29660efe6cd9abc78c4ff396a3c7afc2ffe2
Minato <jor18madrid@gmail.com>
2018-10-31 10:36:29 -0600

    da

--- F.FINANCIAMIENTO/F.FINANCIAMIENTO.WCF/Util/ExecuteQuery.cs
+++ F.FINANCIAMIENTO/F.FINANCIAMIENTO.WCF/Util/ExecuteQuery.cs
@@ -45,13 +45,14 @@ namespace F.FINANCIAMIENTO.WCF.Util
                         {
                             item.VIGENTE = "S";
                         }
-                        cmd.CommandText = "insert into USER_TALLERAWS.FUENTES_FINANCIAMIENTO (FUENTE,GRUPO_FUENTE,SUB_GRUPO_FUENTE,DESC_FUENTE,VIGENTE,API_ESTADO,API_TRANSACCION,USU_CRE,FEC_CRE,USU_MOD,FEC_MOD) values " + "(" + item.FUENTE_FINANCIAMIENTO + "," + item.ID_GRUPO + "," + item.SUB_GRUPO_FUENTE + "," + "'" + item.DESC + "'" + "," + "'" + item.VIGENTE + "'" + "," + "'ELABORADO','CREAR',user,sysdate,NULL,NULL)";
+                        cmd.CommandText = "insert into USER_TALLERAWS.FUENTES_FINANCIAMIENTO (FUENTE,GRUPO_FUENTE,SUB_GRUPO_FUENTE,DESC_FUENTE,VIGENTE,API_ESTADO,API_TRANSACCION,USU_CRE,FEC_CRE) values " + "(" + item.FUENTE_FINANCIAMIENTO + "," + item.ID_GRUPO + "," + item.SUB_GRUPO_FUENTE + "," + "'" + item.DESC + "'" + "," + "'" + item.VIGENTE + "'" + "," + "'ELABORADO','CREAR','SIAFI',sysdate)";
                         cmd.ExecuteNonQuery();
                         insCont++;
                     }
 
-                   /*Cambiar por el commit*/transaction.Rollback();
-                    //transaction.Commit();
+                    /*Cambiar por el commit*/
+                    //transaction.Rollback();
+                    transaction.Commit();
                     msj = "Query ejecutado con éxito!,      " +
                         "\n Se realizaron " + insCont + " acciones";
                     insCont = 0;
@@ -103,19 +104,15 @@ namespace F.FINANCIAMIENTO.WCF.Util
 
                     foreach (var item in nUpd)
                     {
-
-
-
-
-                        //cmd.CommandText = "update USER_TALLERAWS.FUENTES_FINANCIAMIENTO (FUENTE,GRUPO_FUENTE,SUB_GRUPO_FUENTE,DESC_FUENTE,VIGENTE,API_ESTADO,API_TRANSACCION,USU_CRE,FEC_CRE,USU_MOD,FEC_MOD) values " + "(" + item.FUENTE_FINANCIAMIENTO + "," + item.ID_GRUPO + "," + item.SUB_GRUPO_FUENTE + "," + "'" + item.DESC + "'" + "," + "'" + item.VIGENTE + "'" + "," + "'ELABORADO','CREAR',user,sysdate,NULL,NULL)";
-                        //cmd.ExecuteNonQuery();
-                        //insCont++;
+                        cmd.CommandText = "update USER_TALLERAWS.FUENTES_FINANCIAMIENTO (FUENTE,GRUPO_FUENTE,SUB_GRUPO_FUENTE,DESC_FUENTE,VIGENTE,API_ESTADO,API_TRANSACCION,USU_CRE,FEC_CRE,USU_MOD,FEC_MOD) values " + "(" + item.FUENTE_FINANCIAMIENTO + "," + item.ID_GRUPO + "," + item.SUB_GRUPO_FUENTE + "," + "'" + item.DESC + "'" + "," + "'" + item.VIGENTE + "'" + "," + "'ELABORADO','CREAR',user,sysdate,NULL,NULL)";
+                        cmd.ExecuteNonQuery();
+                        insCont++;
                     }
 
                     /*Cambiar por el commit*/
-                    transaction.Rollback();
+                    //transaction.Rollback();
                     //transaction.Commit();
-                    msj = "Query ejecutado con éxito!,      " +
+                    msj = "Registros actualizados con éxito!,      " +
                         "\n Se realizaron " + insCont + " acciones";
                     insCont = 0;
                     cmd.Dispose();
--- F.FINANCIAMIENTO/F.FINANCIAMIENTO.WEB/Controllers/HomeController.cs
+++ F.FINANCIAMIENTO/F.FINANCIAMIENTO.WEB/Controllers/HomeController.cs
@@ -20,41 +20,28 @@ namespace F.FINANCIAMIENTO.WEB.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            //var grupos = _fGrupoFiltro.qry_GRUPOS_FUENTES_FIN_Listado();
-            //var dicGrpsLst = new Dictionary<int, string>();
-            //foreach (var item in grupos)
-            //{
-            //    var id = item;
-            //    dicGrpsLst.Add(item.GRUPO_FUENTE, item.DESC_GRUPO_FUENTE.ToString());
-            //}
-            //ViewBag.Grupos = dicGrpsLst.Select(x => new SelectListItem { Value = x.Key.ToString(), Text = x.Value });
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////
-            var dicGrpsLst = new List<SAS_GRUPO_FILTRO_DTO>();
-            dicGrpsLst.Add(new SAS_GRUPO_FILTRO_DTO() { ID_GRUPO = 1, NOMBRE_GRUPO = "Grupo 1" });
-            dicGrpsLst.Add(new SAS_GRUPO_FILTRO_DTO() { ID_GRUPO = 2, NOMBRE_GRUPO = "Grupo 2" });
-            ViewBag.Grupos = dicGrpsLst.Select(x => new SelectListItem { Value = x.ID_GRUPO.ToString(), Text = x.NOMBRE_GRUPO });
-            //////////////////////////////////////////////////////////////////////////////////////////////////////////
-            ///////////////////////////////////////
-            ViewBag.Result = ExecuteQuery.result;
-            ExecuteQuery.result = "";
+            var grupos = _fGrupoFiltro.qry_GRUPOS_FUENTES_FIN_Listado();
+            var dicGrpsLst = new Dictionary<int, string>();
+            foreach (var item in grupos)
+            {
+                var id = item;
+                dicGrpsLst.Add(item.GRUPO_FUENTE, item.DESC_GRUPO_FUENTE.ToString());
+            }
+            ViewBag.Grupos = dicGrpsLst.Select(x => new SelectListItem { Value = x.Key.ToString(), Text = x.Value });
+            
             return View();
-            //////////////////////////////////////
         }
 
         [HttpPost]
-        public ActionResult GetData(INSERTDTO data/*,UPDATEDTO updData*/)
+        public ActionResult GetData(INSERTDTO data)
         {
-
-            //var nUpd = updData.FuenteFinUpd.Where(x => x.FFNUPD == "S").OrderBy(y => y.FUENTEFUPD);
-            //ExecuteQuery.result = ExecuteQuery.Execute(data);
-            return Json("Retorna del controlador GUARDAR", JsonRequestBehavior.DenyGet);
+            return Json(ExecuteQuery.Execute(data), JsonRequestBehavior.DenyGet);
         }
 
         [HttpPost]
         public ActionResult Update(UPDATEDTO updData)
         {
-            //Update_FFinanciamiento.Execute(updData);
-            return Json("Retorna del controlador UPDATE ", JsonRequestBehavior.DenyGet);
+            return Json(Update_FFinanciamiento.Execute(updData), JsonRequestBehavior.DenyGet);
         }
 
 
@@ -62,12 +49,8 @@ namespace F.FINANCIAMIENTO.WEB.Controllers
         [HttpGet]
         public JsonResult getFFinanciamiento()
         {
-            //var fsFinanciamiento = _fGrupoFiltro.qry_FUENTES_FINANCIAMIENTO_Listado();
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            List<FUENTES_FINANCIAMIENTO_DTO> fsFinanciamiento = new List<FUENTES_FINANCIAMIENTO_DTO>();
-            fsFinanciamiento.Add(new FUENTES_FINANCIAMIENTO_DTO() { FUENTE = 12, VIGENTE = "S", DESC_FUENTE = "Mixto" });
-            fsFinanciamiento.Add(new FUENTES_FINANCIAMIENTO_DTO() { FUENTE = 32, VIGENTE = "N", DESC_FUENTE = "Mixto2" });
-            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            var fsFinanciamiento = _fGrupoFiltro.qry_FUENTES_FINANCIAMIENTO_Listado();
+           
             return Json(fsFinanciamiento, JsonRequestBehavior.AllowGet);
         }
 
